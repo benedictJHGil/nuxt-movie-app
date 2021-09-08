7c2966f2e9b437d6c2bfd31afdba53ef01a0bc0d
benedictJHGil <benedictJHGil@gmail.com>
2021-09-08 11:34:14 +0900

    Intiate nuxt project

---
+++ store/about.js
@@ -0,0 +1,9 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'benedictJHGil',
+    email: 'benedictJHGil@gmail.com',
+    phone: '+82-10-5424-2374',
+    image: 'https://raw.githubusercontent.com/benedictJHGil/vue3-movie-app/master/src/assets/logo.png'
+  })
+}
